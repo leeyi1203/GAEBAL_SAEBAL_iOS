e5654f707ee4aa8532f031a851fd3158de7eb265
Leebomyi <tak1203@naver.com>
2022-04-06 19:24:32 +0900

    MVC 구분 및 탭바,네비게이션바 생성

---
+++ 개발새발/GaebalSaebal/Controller/MyPage/MyPageViewController.swift
@@ -0,0 +1,29 @@
+//
+//  MyPageViewController.swift
+//  GaebalSaebal
+//
+//  Created by 이봄이 on 2022/04/06.
+//
+
+import UIKit
+
+class MyPageViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
